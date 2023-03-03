71b510311ed5b06de8319a6f4e53defd18b81ea7
jonesro2 <102373183+jonesro2@users.noreply.github.com>
2023-03-02 21:44:31 -0500

    Update editGenome.sh

--- editGenome.sh
+++ editGenome.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Author: Robin Jones
-#RBIF100:Identifying Potential Crispr Genes in a Cohort
+#Identifying Potential Crispr Genes in a Cohort
 file1=./*_precrispr.fasta
 #loop through each precrispr file,save code name,find .GG and add A before that pattern(&)
 #output edited genes to postcrispr file
